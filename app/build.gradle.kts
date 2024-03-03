c97ed4a56038c1a4a3dddded5fcfb2ecd245c8b1
k.butusov <konstantin.butusov@yandex.ru>
2024-03-03 19:20:08 +0700

    Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.testeducation.education"
+        applicationId = "com.testoria.app"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
