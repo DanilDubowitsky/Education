878560f47e32d23f9ffb512a138a1e4a29302e1e
k.butusov <konstantin.butusov@yandex.ru>
2024-03-03 18:54:36 +0700

    Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "0.0.1"
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
