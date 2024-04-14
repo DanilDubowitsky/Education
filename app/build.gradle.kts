3dddc874d516c54d10669dbcbd96528900228585
yolomeisterx <danil.dubowitsky@gmail.com>
2024-04-14 18:02:37 +0700

    feat: update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.testoria.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.1.1"
+        versionCode = 4
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
